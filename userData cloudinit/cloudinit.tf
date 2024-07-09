52047e73b43cdd4b4ab00ee3e5bae3b62b5eaf33
jatannoury-Tradias <j.tannoury.ext@bankhaus-scheich.de>
2024-07-09 20:57:49 +0300

    adding user data using cloud init

---
+++ userData cloudinit/cloudinit.tf
@@ -0,0 +1,13 @@
+data "template_file" "install_appache" {
+  template = file("init.cfg")
+}
+
+data "template_cloudinit_config" "install_appache_config" {
+  gzip = false
+  base64_encode = false
+  part {
+    filename = "init.cfg"
+    content_type = "text/cloud-config"
+    content = data.template_file.install_appache.rendered
+  }
+}
---
+++ userData cloudinit/create_instance.tf
@@ -0,0 +1,39 @@
+resource "aws_key_pair" "levelup_key"{
+  key_name = "levelup_key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "example_instance" {
+  ami = lookup(var.AMIs, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.levelup_key.key_name
+  availability_zone = "us-east-1a"
+  user_data = data.template_cloudinit_config.install_appache_config.rendered
+
+  tags = {
+    Name = "demoinstance"
+  }
+  
+}
+
+# Create EBS
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "us-east-1a"
+  size = 50
+  type = "gp2"
+
+  tags = {
+    Name = "Secondary volume Disk"
+  }
+}
+
+#attach EBS volume with AWS instance
+resource "aws_volume_attachment" "aws-volume-1-attachment" {
+  device_name = "/dev/xvdh"
+  volume_id = aws_ebs_volume.ebs-volume-1.id
+  instance_id = aws_instance.example_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.example_instance.public_ip
+}
